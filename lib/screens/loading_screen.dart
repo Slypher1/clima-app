30f201ffb9a228917b829ce1bfda34264d8a8a4b
Slypher <william-balcazar@outlook.com>
2020-03-08 22:50:37 +0100

    Conclusa lezione 9

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,16 +9,33 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
   void getLocation() async {
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    Location location = Location();
+    await location.getCurrentLocation();
+    print('Latitude: ${location.latitude}');
+    print('Longitude: ${location.longitude}');
+  }
+
+  void getData() async {
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    if(response.statusCode == 200) {
+      String data = response.body;
+
+      var decodeData = jsonDecode(data);
+      double temperature = decodeData['main']['temp'];
+      int condition = decodeData['weather'][0]['id'];
+      String cityName = decodeData['name'];
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    getLocation();
+    //getLocation();
+    getData();
   }
 
   @override
